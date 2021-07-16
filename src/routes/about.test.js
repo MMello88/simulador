32d476c7219163e3207454e28e1833335a528650
MattMello88 <matheusnarciso@hotmail.com>
2021-07-16 19:02:16 -0300

    test criado para products

---
+++ src/routes/about.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from "test-utils";
+
+import About from "./about";
+
+
+
+
+test('scrolls to top in first render', () =>{
+  render(<About></About>);
+  expect(window.scrollTo).toBeCalledTimes(1);
+})
